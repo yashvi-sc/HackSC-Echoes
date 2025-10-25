dc3ecb870d3f3740000681e30d87129631b39c77
yashvi-sc <yhiranan@usc.edu>
2025-10-25 11:32:42 -0700

    Commit

--- backend/test2.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import mediapipe as mp
-import cv2
-from deepface import DeepFace
-
-print("TensorFlow:", tf.__version__)
-print("MediaPipe:", mp.__version__)
-print("OpenCV:", cv2.__version__)
-DeepFace.stream(db_path=None)
